6bbc852505406326f10f73f27c87f6a7f3b4b830
sakib01773 <sakibshohan1@gmail.com>
2022-11-21 14:17:05 +0600

    dtabase connection and load appontmens options

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
 const app = express()
@@ -10,6 +12,26 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.aebmoeb.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri);
+
+async function run() {
+  try {
+    const appointmentoptionCollection = client.db("doctorsPortal").collection("AppoinmentOption");
+
+    app.get("/appoinmentOptions", async (req, res) => {
+      const query = {};
+      const options = await appointmentoptionCollection.find(query).toArray();
+      res.send(options)
+    })
+      
+    
+  } finally {
+
+  }
+}
+run().catch(err => console.log(err));
 
 app.get('/', async(req, res) => {
   res.send('Doctors Portal is Running')
